7fe47cb4fadc13d9a38f2409d39d9dc8a0e4ef12
Semen Muzalevskyi <muzalevskyi.sema@mail.ru>
2024-07-10 11:15:38 +0700

    Исправил изменения

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 const close1 = document.querySelector(".pop-up-close");
 const popUp = document.querySelector(".pop-up");
 
 close1.addEventListener("click", () => {
     popUp.classList.add("_hidden")
-=======
-const btnFormClose = document.querySelector(".pop-up-close");
-const popup = document.querySelector(".pop-up")
-
-btnFormClose.addEventListener("click", () => {
-    popup.style.display = "none"
->>>>>>> ff7d5f8bd82517a3c950f26e86f77ea87a6c9dea
 })
